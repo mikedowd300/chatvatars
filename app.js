4b3c71162d23302f40c1ae8b0537663ee18b1b80
Mike Dowd <mikedowd1972@gmail.com>
2017-12-31 18:51:01 -0700

    finished the message submit feature

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 $(document).ready(() => {
 
   $('.new-message').keyup((event) => {
-    if(event.which === 13) {
-      let message = $('.new-message').val();
-      let element = `<div class="sent-message border"><p>${message}</p></div>`;
-      $('.messages-wrapper').prepend(element);
-      $('.new-message').val('');
-    }
+    postSentMessage();
   });
 
 });
+
+postSentMessage = () => {
+  if(event.which === 13) {
+    let message = $('.new-message').val();
+    let element = `<div class="sent-message"><p>${message}</p></div>`;
+    $('.messages-wrapper').prepend(element);
+    $('.new-message').val('');
+  }
+}
