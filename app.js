8d6394ac00f038d4331ab0c1ba9c0785c8870fd6
Mike Dowd <mikedowd1972@gmail.com>
2018-01-02 20:37:06 -0700

    oops

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 $(document).ready(() => {
 
+  let serverUrl = 'http://127.0.0.1:4200';
+
+  try {
+    var socket = io.connect(serverUrl);
+  }
+  catch(e) {
+    console.log("Couldn't connect!!!");
+  }
+
+  if( socket !== undefined) {
+    socket.on('say-hi', (data) => {
+      console.log(data);
+      displayAvatars();
+    })
+  }
+
+
   $('.new-message').keyup((event) => {
     postSentMessage();
   });
