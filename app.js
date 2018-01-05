89c51bf869c94fb3d82c370933dd8d6f3bf4eff3
Mike Dowd <mikedowd1972@gmail.com>
2018-01-04 20:20:29 -0700

    did more with the click event on the avatar

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ $(document).ready(() => {
     message: ''
   }
 
+  let isActive = false;
+
   let serverUrl = 'http://127.0.0.1:4200';
 
   try {
@@ -28,22 +30,30 @@ $(document).ready(() => {
   displayAvatars = (avs) => {
     avs.forEach( av => {
       let $element = $(`<div class="image-wrapper circle"><img class="avatar-image" src="${av}"></div>`);
-
-      $element.click(() => {
-        console.log('hello');
-        chatter.avatar = av;
-      });
-
+      createAvatarClickedEvent($element, av);
       $('.avatar-wrapper').append($element);
     });
   }
 
+  createAvatarClickedEvent = ($elem, av) => {
+    $elem.click(() => {
+      chatter.avatar = av;
+      $('.new-message').attr('placeholder', '');
+      isActive = true;
+    });
+  }
+
   postSentMessage = () => {
-    if(event.which === 13) {
+    if(event.which === 13 && isActive) {
+      $('.nasty-message').addClass('hidden');
       let message = $('.new-message').val();
       let element = `<div class="sent-message border flexer"><p>${message}</p><img class="avatar-mini circle border" src="${chatter.avatar}"></div>`;
       $('.messages-wrapper').prepend(element);
       $('.new-message').val('');
+    } else if(event.which === 13 && !isActive ) {
+      console.log('wrong!!!!');
+      let $message = $(`<p class="nasty-message">YOU MUST FIRST SELECT AN AVATAR!!!</p>`);
+      $('.messages-wrapper').prepend($message);
     }
   }
 
